2a6af44ea3ce0a391feaf24057fae0096b4b0a39
xxxmoney <xxxmoney111@gmail.com>
2023-01-21 15:59:29 +0100

    Functional scrapping, better config, logs, other.

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-import { Scraper } from './src/scraper.js';
+import { Scraper, Configuration } from './src/scraper.js';
+import fs from 'fs';
 
 (async () => {
-    const scraper = new Scraper();
+    const replaceSpaces = (value) => value.replace(/ /g, "");
+
+    const config = {
+        pageUrl: "https://www.sportvokoli.cz/sportovni-kluby",
+        linksLimitOverride: 5,
+        loadNextSelector: ".txt-c a",
+        loadNextInterval: 10,
+        setDescriptionInterval: 10,
+        linksSelector: "#lister-rows a",
+        linksCountInfoSelector: "map-results h1",
+        linksCountInfoRegex: /\d{2} \d{3}/,
+        descriptionOptions: [
+            { name: "name", selector: ".club-name" },
+            { name: "address", selector: ".box .dlist *:nth-child(4)", transformation: (value) => value.replace("\n\nNAPLÁNOVAT TRASU", "")  },
+            { name: "phone", selector: ".box .dlist *:nth-child(6)", regex: /(?:\+(?:\d ?){3})?(?:\d ?){9}/, transformation: replaceSpaces },
+            { name: "email", selector: ".box .dlist *:nth-child(6)", regex: /[\w.]+@[\w.]+/, transformation: replaceSpaces },
+            { name: "website", selector: ".box .dlist *:nth-child(8) a" },
+            { name: "sportType", selector: ".box .dlist *:nth-child(2)" }        
+        ]
+    };
+    const scraper = new Scraper(config);    
     await scraper.init();
 
-    // Do something with scraper
+    try {
+        await scraper.setLinks();
+        await scraper.setDescriptions();
+    } catch (error) {
+        console.error(error);
+    }
 
     await scraper.close();
+    
+
+    // Saves links and errors to file.
+    const linksJson = JSON.stringify(scraper.links);
+    const errorsJson = JSON.stringify(scraper.errors);
+    fs.writeFileSync('log\\links.json', linksJson);
+    fs.writeFileSync('log\\errors.json', errorsJson);
 })();
+
--- src/scraper.js
+++ src/scraper.js
@@ -1,19 +1,242 @@
-import puppeteer from 'puppeteer';
+import { Browser, Page, launch } from 'puppeteer';
+
+class Configuration {
+    pageUrl;
+    linksLimitOverride = null;
+    loadNextSelector;
+    loadNextInterval;
+    linksSelector;
+    setDescriptionInterval;
+    linksCountInfoSelector;    
+    linksCountInfoRegex;    
+    /** @type {Array.<ConfigurationDescriptionItem>} */
+    descriptionOptions;
+}
+class ConfigurationDescriptionItem {
+    name;
+    selector;
+    regex = null;
+    transformation = null;
+}
+
+class Link {
+    url;
+    /** @type {Description} */
+    description;
+
+    constructor(url) {
+        this.url = url;
+        this.description = new Description();
+    }
+}
+class Description {
+    /** @type {Array.<DescriptionItem>} */
+    items;
+    
+    constructor() {
+        this.items = [];
+    }
+
+    /** 
+     * Adds description item to description.
+     * @param {DescriptionItem} item 
+    */
+    add(item) {
+        this.items.push(item);
+    }
+}
+class DescriptionItem {
+    name;
+    value;
+
+    constructor(name, value) {
+        this.name = name;
+        this.value = value;
+    }
+}
+
+class Error {
+    url;
+    name;
+    message;
+
+    constructor(url, name, message) {
+        this.url = url;
+        this.name = name;
+        this.message = message;
+    }   
+
+    toString() {
+        return `${this.name}: ${this.message} (${this.url})`;
+    }
+}
 
 class Scraper {
+    /** @type {Browser} */
     #browser;
+    /** @type {Array.<Link>} */
+    #links;
+    /** @type {Array.<Error>} */
+    #errors;
+    /** @type {Configuration} */
+    #cofiguration;
 
-    constructor() {
+    constructor(configuration) {
         this.#browser = null;
+        this.#links = [];
+        this.#errors = [];
+        this.#cofiguration = configuration;
     }
 
+    /** Initializes scraper. */
     async init() {
-        this.#browser = await puppeteer.launch();        
-    }
+        this.#browser = await launch(); 
+    } 
+    /** Closes scraper. */
     async close() {
         await this.#browser.close();
     }    
+     
+    get links() {        
+        return this.#links;
+    }
+    get errors() {        
+        return this.#errors;
+    }
+
+    /** 
+     * Gets links count from page. 
+     * @param {Page} page
+    */
+    async #getLinksCount(page) {
+        const linksCountInfo = await page.evaluate((selector) => {
+            const element = document.querySelector(selector);
+            return element ? element.innerText : null;
+        }, this.#cofiguration.linksCountInfoSelector);
+
+        if (linksCountInfo) {
+            const match = linksCountInfo.match(this.#cofiguration.linksCountInfoRegex);
+            if (match) {
+                return parseInt(match[0].replace(" ", ""));
+            }
+
+            throw new Error("Links count info does not match regex.");
+        }
+
+        throw new Error("Links count info not found.");
+    }    
+    /** 
+     * Loads next links. 
+     * @param {Page} page
+    */
+    async #loadNext(page) {
+        await page.evaluate((selector) => {
+            const element = document.querySelector(selector);
+            if (element) {
+                element.click();
+            }
+        }, this.#cofiguration.loadNextSelector);
+
+        // Timeout with promise
+        await new Promise(resolve => setTimeout(resolve, this.#cofiguration.loadNextInterval));        
+    }
+    /** 
+     * Returns links' hrefs. 
+     * @param {Page} page
+    */
+    async #getLinkHrefs(page) {
+        return await page.evaluate((selector) => {
+            const anchors = Array.from(document.querySelectorAll(selector));
+            return anchors.map(anchor => anchor.href);
+        }, this.#cofiguration.linksSelector);
+    }
+    
+    /** Gets links from page. */
+    async setLinks() {
+        const page = await this.#browser.newPage();
+        await page.goto(this.#cofiguration.pageUrl);
+
+        // Get links count.
+        const limit = this.#cofiguration.linksLimitOverride ?? await this.#getLinksCount(page);
+        
+        // Keeps loading next page until all links are loaded.
+        let hrefs = [];
+        while ((hrefs = await this.#getLinkHrefs(page)).length < limit) {
+            await this.#loadNext(page);
+        }        
+
+        // Sets links from hrefs.
+        this.#links = hrefs.map(href => new Link(href));
+
+        await page.close();
+    }
+
+    /** 
+     * Sets descriptions for link. 
+     * @param {String} descriptionUrl
+     * @param {Link} link
+    */
+    async #setDescription(link) {
+        const page = await this.#browser.newPage(link.url);
+        await page.goto(link.url);
+
+        // Goes through all description options and sets description.
+        for (let i = 0; i < this.#cofiguration.descriptionOptions.length; i++) {
+            const option = this.#cofiguration.descriptionOptions[i];
+
+            let value = await page.evaluate((selector) => {
+                const element = document.querySelector(selector);
+                return element ? element.innerText : null;
+            }, option.selector);
+
+            // Checks whether value has value.
+            if(!value) {
+                const error = new Error(link.url, option.name, "Description value not found.");
+                this.#errors.push(error);
+                console.error(error.toString());
+                continue;
+            }
+
+            // Checks whether value matches regex.
+            if(option.regex) {
+                const match = value.match(option.regex);
+                if(!match) {
+                    const error = new Error(link.url, option.name, "Description value does not match regex.");
+                    this.#errors.push(error);
+                    console.error(error.toString());
+                    continue;
+                }
+                value = match[0];
+            }
+
+            // Checks whether value needs transformation.
+            if(option.transformation) {
+                value = option.transformation(value);
+            }
+
+            const item = new DescriptionItem(option.name, value);
+            link.description.add(item);            
+        }
+
+        await page.close();
+    }
+
+    /** Gets descriptions from loaded links. */
+    async setDescriptions() {        
+       // Goes through all links and sets description.
+        //  await Promise.all(this.#links.map(async link => {
+        //     await this.#setDescription(link.url, link);
+        // }));        
+
+        // Goes through all links and sets description.
+        for (let i = 0; i < this.#links.length; i++) {
+            await this.#setDescription(this.#links[i]);
+
+            // Timeout with promise.
+            await new Promise(resolve => setTimeout(resolve, this.#cofiguration.setDescriptionInterval));
+        }
+    }
     
 }
 
-export { Scraper };
+export { Scraper, Configuration };
